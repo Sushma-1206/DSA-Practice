8caa543c8dd34befadfe54e728242e334c66f197
Sushma-stack-hub <sushmakumari@sushmas-MacBook-Air-2.local>
2026-03-28 21:57:40 +0530

    Add Single Number (XOR Appraoch)

---
+++ Arrays/singleNumber.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result=0
+        for num in nums:
+            result=result^num
+        return result
